824c4560038ce8f1aaf38a9084ae38268fc248d0
Shahzaib <93034224+SyedShahzaibAlam@users.noreply.github.com>
2023-08-20 15:51:20 +0500

    Update server.js

--- server.js
+++ server.js
@@ -11,15 +11,7 @@ dotenv.config();
 
 //mongodb connection
 connectDB();
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
-}
+
 
 //rest object
 const app = express();
